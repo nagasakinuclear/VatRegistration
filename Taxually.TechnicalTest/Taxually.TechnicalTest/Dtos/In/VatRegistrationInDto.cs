2d5493131ee1a29ea006c0f90134e29031d47b6e
adandreyad <nagasakinuclear@gmail.com>
2022-09-08 01:20:03 +0300

    Initial and project structure

---
+++ Taxually.TechnicalTest/Taxually.TechnicalTest/Dtos/In/VatRegistrationInDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Taxually.TechnicalTest.Dtos.In
+{
+    public class VatRegistrationRequest
+    {
+        public string? CompanyName { get; set; }
+        public string? CompanyId { get; set; }
+        public string? Country { get; set; }
+    }
+}
